3299b8f11c362bcbb5761126872ca03e3717f295
Kundan Kumar <kun9577@gmail.com>
2024-11-25 18:26:47 +0530

    css modified

--- src/components/PermissionsList.js
+++ src/components/PermissionsList.js
@@ -6,20 +6,26 @@ const PermissionList = ({ roles, onPermissionChange }) => {
   
   return (
     <div className="permissions-list">
+
+
+
+      
       <div className="title">Permissions</div>
       {roles.map((role) => (
         <div key={role.id} className="role-permissions">
           <div className="rolename">{role.name}</div>
           <div className="permissions-checkboxes">
             {allPermissions.map((permission) => (
-              <label key={permission} className="checkbox-label">
+              <div key={permission} className="checkbox-container">
+              <div className="checkbox-wrapper">
                 <input
                   type="checkbox"
                   checked={role.permissions.includes(permission)}
                   onChange={() => onPermissionChange(role.id, permission)}
                 />
-                {permission}
-              </label>
+              </div>
+              <div className="checkbox-text">{permission}</div>
+              </div>
             ))}
           </div>
         </div>
